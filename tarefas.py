68d6b7ed472b622fb5c4091f4bb92c26cb633ba9
leandrodev-sudo <leandrohenriquedev@gmail.com>
2026-06-03 17:21:47 -0300

    add func de tarefas

---
+++ tarefas.py
@@ -0,0 +1,17 @@
+def cadastrar_tarefa():
+    pass
+
+def editar_tarefa():
+    pass
+
+def excluir_tarefa():
+    pass
+
+def calcular_prazo():
+    pass
+
+def mostrar_alertas():
+    pass
+
+def testar_datas():
+    pass
