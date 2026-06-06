fce8ef4e874ef295d72f0e18261cb84240609a28
Miguel Cabal <mmiguelcabra@gmail.com>
2026-06-06 14:04:56 -0300

    Primeiro menu terminado, sera revisado depois

--- interface.py
+++ interface.py
@@ -4,31 +4,107 @@ from crud_animais import (
     cadastrar_animal,
     listar_animais,
     editar_animal,
-    excluir_animal
+    excluir_animal,
+    _idade_valida, 
+    _animal_duplicado,
+    
 )
 
-#PRIMEIRA PARTE (pra limpar e deixar bonitinho)
+# PRIMEIRA PARTE (pra limpar e deixar bonitinho)
 def limpar():
     print("\n" * 10)
-def pausar():
-    pausa= int (input("Bem vindo a interface do Adoção+! Caso queira continuar, pressione 1, caso contrário, pressione 2: "))
-    if pausa==2:
-        exit()
-    if pausa==1:
-        print (menu_geral)
-    else:
-        print ("Digite apenas UM dos NÚMEROS indicados.")
-#PARTE DO MENU GERAL (Basicamente vai ter as escolhas que o usuário vai fazer, enfim.)
 
+def pausar():
+    while True: 
+        try: #tente, puxar esses ifs e elifs e else lol
+            pausa = int(input("Bem vindo a interface do Adoção+! Caso queira continuar, pressione 1, caso contrário, pressione 2: "))
+            if pausa == 2:
+                print("Saindo... Até logo!")
+                exit()
+            elif pausa == 1:
+                menu_geral()  
+                break         
+            else:
+                print("Digite apenas UM dos NÚMEROS indicados.\n")
+        except ValueError: #caso o usuário BURRO digite alguma LETRA
+            print("Por favor, digite um número válido (1 ou 2).\n")
+
+# PARTE DO MENU GERAL
 def menu_geral():
-    
-      while True:
-         limpar()
-         print ("✹"*30)
-         print ("SEJA BEM VINDO AO ADOTE+!!!\n\n\n")   
-         print("Selecione abaixo a tarefa que você deseja!")
-         print ("✹"*30)
+    while True:
+        limpar()
+        print("✹" * 30)
+        print("SEJA BEM VINDO AO ADOTE+!!!\n")  
+        print("Este é o menu principal.\n") 
+        print("Selecione abaixo o menu que você deseja acessar! ")
+        print("✹" * 30)
         
+      
+        
+        print("1 - Acessar o menu de Animais")
+        print("2 - Acessar o menu de Matching")
+        print("3 - Acessar o menu de Sugestões")
+        print("4 - Acessar o menu de Tarefas")
+        print("0 - Sair")
+        print("✹" * 30)
+        
+        try:
+            opcao = input("Escolha uma opção: ")
+        
+            if opcao == "1":
+             print ("\n"*5)
+             print("✹" * 30)
+             print ("BEM VINDO AO MENU DE ANIMAIS! O QUE VOCÊ DESEJA?")
+             print("✹" * 30)
+             print ("1- Registrar Animal")
+             print ("2- Verificar a Lista de Animais Registrados")
+             print ("3- Editar Animais Registrados")
+             print ("4- Excluir Animal Registrado")
+             print ("0- Sair do menu")
+             print("✹" * 30)
+             resposta_menu_animais=int(input("Digite sua resposta: "))
+             if resposta_menu_animais==1:
+              cadastrar_animal()
+              pausar()
+             elif resposta_menu_animais== "2":
+              listar_animais()
+              pausar()
+             elif resposta_menu_animais == "3":
+              editar_animal()
+              pausar()
+             elif resposta_menu_animais == "4":
+              excluir_animal()
+              pausar()
+             elif resposta_menu_animais == "0":
+              print("Saindo do sistema... Até logo!")
+              break 
+             else:
+              input("Opção inválida! Pressione Enter para tentar novamente...")
+
+            if opcao == "2":
+             print ("\n"*5)
+             print("✹" * 30)
+             print ("BEM VINDO AO SISTEMA DE MATCHING! O QUE VOCÊ DESEJA?")
+             print("✹" * 30)
+            if opcao == "3":
+             print ("\n"*5)
+             print("✹" * 30)
+             print ("BEM VINDO AO MENU DE SUGESTÕES! O QUE VOCÊ DESEJA?")
+             print("✹" * 30)
+
+            if opcao == "4":
+             print ("\n"*5)
+             print("✹" * 30)
+             print ("BEM VINDO AO MENU DE TAREFAS! O QUE VOCÊ DESEJA?")
+             print("✹" * 30)
+            
+        except ValueError:
+           print ("Digite APENAS NÚMEROS")
+           continue
+        
+# Execução do programa
+pausar()
+
 #             if resposta==0:
 #                 break
 #         elif resposta==1:
